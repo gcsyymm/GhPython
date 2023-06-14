7dd576bfacfeee79fee249ca3a8b3d604d104492
gcsyymm <128434215+gcsyymm@users.noreply.github.com>
2023-06-14 09:45:41 +0800

    Add files via upload

--- Select_linear_path.py
+++ Select_linear_path.py
@@ -1,1 +1,6 @@
-﻿a = ["{};{}".format(i,j) for j in range(len(x.Paths)) if x.PathExists(i,j)]
+﻿j,k = 0, 0
+a = ["{};{}".format(i,j) for j in range(len(x.Paths)) if x.PathExists(i,j)]
+# a = selected paths with i line
+# swap i,j for different selection
+# more genal case
+b = ["{};{};{}".format(i,j,k) for j in range(len(x.Paths)) for k in range(len(x.Paths)) if x.PathExists(i,j,k)]
---
+++ Shift_Tree_Path.py
@@ -0,0 +1,6 @@
+﻿a = list(x.Paths) 
+#a = path index
+s = int(s % len(a)) 
+#s = shift
+b = a[s:] + a[:s] if w else a[s:] 
+#b = shifted path, w = wrap boolean
