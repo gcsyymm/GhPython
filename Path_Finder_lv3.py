5f4ab073434f55fd36b6aba18af9e9f20cfd7c2a
gcsyymm <128434215+gcsyymm@users.noreply.github.com>
2023-06-16 10:43:38 +0800

    Add files via upload

---
+++ Path_Finder_lv3.py
@@ -0,0 +1,8 @@
+﻿j = k = int(0)
+i = int(i)
+for j in range(x.BranchCount):
+    for k in range(x.BranchCount):
+        if x.PathExists(i,j,k):
+            print('{'+'{};{};{}'.format(i,j,k)+'}')
+        else:
+            break
