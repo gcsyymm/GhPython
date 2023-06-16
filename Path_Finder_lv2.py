52c083232b3bd6208b160bbaa05b32b9f3b1fc65
gcsyymm <128434215+gcsyymm@users.noreply.github.com>
2023-06-16 10:40:01 +0800

    Update Path_Finder_lv2.py

--- Path_Finder_lv2.py
+++ Path_Finder_lv2.py
@@ -1,10 +1,8 @@
-﻿j = int(0)
+j = int(0)
 i = int(i)
 y = list()
 for j in range(len(x.Paths)):
     if x.PathExists(i,j):
-        y.append("{"+"{};{}".format(i,j)+"}")
+        print("{"+"{};{}".format(i,j)+"}")
     else:
         break
-for _ in range(len(y)):
-    print(y[_])
