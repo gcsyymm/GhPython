09c5802ab27c02c8153b575559cee4972a64c3a8
gcsyymm <128434215+gcsyymm@users.noreply.github.com>
2023-06-16 10:37:55 +0800

    Add files via upload

---
+++ Path_Finder_lv2.py
@@ -0,0 +1,10 @@
+﻿j = int(0)
+i = int(i)
+y = list()
+for j in range(len(x.Paths)):
+    if x.PathExists(i,j):
+        y.append("{"+"{};{}".format(i,j)+"}")
+    else:
+        break
+for _ in range(len(y)):
+    print(y[_])
