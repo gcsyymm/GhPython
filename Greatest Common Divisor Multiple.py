157149ebaee59421093bd3bb2f70e53fe29f3a9b
gcsyymm <128434215+gcsyymm@users.noreply.github.com>
2023-06-29 15:35:43 +0800

    Add files via upload

---
+++ Greatest Common Divisor Multiple.py
@@ -0,0 +1,17 @@
+﻿if type(x) != list:
+    raise Exception('wrong type for x')
+def gcd(x,y):
+    l = max(x,y)
+    s = min(x,y)
+    r = l % s
+    while r != 0:
+        l = s
+        s = r
+        r = l % s
+    return s
+def gcds(x):
+    ans = gcd(x[0],x[1])
+    for i in xrange(2,len(x)):
+        ans = gcd(ans,x[i])
+    return ans
+print(gcds(x))
